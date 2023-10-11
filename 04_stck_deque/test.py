7bfca7363e82e4957ee02fb679f993af1e3efbaf
Lyubomir Dimitrov <lyubomir.dimitrov.8702@gmail.com>
2023-10-11 12:27:10 +0300

    add files
    exsercise from class

---
+++ 04_stck_deque/test.py
@@ -0,0 +1,9 @@
+from collections import deque
+my_deque = deque([1, 2, 3, 4, 5, 6, 7, 8])
+while my_deque:
+    a = my_deque.popleft()
+    print(a)
+    a += 5
+    print(a)
+    my_deque.appendleft(a)
+    a = my_deque.popleft()
